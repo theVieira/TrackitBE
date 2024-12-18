daed24e48e670d963d0e5445860764de757dfbc4
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-12-18 11:09:15 -0400

    chore: set id to key

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Trackit.Core.Entities;
 
 public class BaseEntity
 {
+  [Key]
   public Guid Id { get; init; } = Guid.NewGuid();
   public DateTime CreatedAt { get; init; } = DateTime.Now;
 }
