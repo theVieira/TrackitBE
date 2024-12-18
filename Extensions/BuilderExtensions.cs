0e3420e9f8441dac5f82037d7ff4b757826eece1
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-12-18 11:27:41 -0400

    feat: configure enums json serialize

--- Extensions/BuilderExtensions.cs
+++ Extensions/BuilderExtensions.cs
@@ -1,9 +1,20 @@
+using System.Text.Json.Serialization;
+
+namespace Trackit.Extensions;
+
 public static class BuilderExtensions
 {
   public static void AddBuilderExtensions(this WebApplicationBuilder builder)
   {
-    builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+      options
+        .JsonSerializerOptions
+        .Converters
+        .Add(new JsonStringEnumConverter());
+    });
   }
 }
