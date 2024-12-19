517d32e266e0bb987441b7c93072a9c249db915e
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-12-19 09:47:44 -0400

    feat: create entity attachment

---
+++ Core/Entities/Attachment.cs
@@ -0,0 +1,22 @@
+namespace Trackit.Core.Entities;
+
+public class Attachment : BaseEntity
+{
+  public string Filename { get; private set; } = string.Empty;
+  public string Path { get; private set; } = string.Empty;
+  public float Size { get; private set; }
+  public Action Author { get; private set; } = new();
+
+  public Attachment() {}
+
+  public Attachment(string filename, string path, float size, Tech author)
+  {
+    if(size < 0)
+      throw new ArgumentException("size cannot be less than 0");
+
+    Filename = filename;
+    Path = path;
+    Size = size;
+    Author = new(author);
+  }
+}
