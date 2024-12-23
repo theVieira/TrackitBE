de96b62724f7252c9c42d31110ca305a37d2d190
theVieira <gabrielvieiraguimaraes4@gmail.com>
2024-12-23 09:08:16 -0400

    feat: initialize migration

---
+++ Migrations/20241220214330_Initialize.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trackit.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialize : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CNPJ = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<bool>(type: "boolean", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Techs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Role = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Techs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Action",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    TicketId = table.Column<Guid>(type: "uuid", nullable: true),
+                    TicketId1 = table.Column<Guid>(type: "uuid", nullable: true),
+                    TicketId2 = table.Column<Guid>(type: "uuid", nullable: true),
+                    TicketId3 = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Action", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Action_Techs_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Techs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SmallId = table.Column<string>(type: "text", nullable: false),
+                    ClientId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OpenId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Category = table.Column<int>(type: "integer", nullable: false),
+                    Priority = table.Column<int>(type: "integer", nullable: false),
+                    Recurrent = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Action_OpenId",
+                        column: x => x.OpenId,
+                        principalTable: "Action",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Attachment",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Filename = table.Column<string>(type: "text", nullable: false),
+                    Path = table.Column<string>(type: "text", nullable: false),
+                    Size = table.Column<float>(type: "real", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TicketId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attachment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attachment_Action_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Action",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Attachment_Tickets_TicketId",
+                        column: x => x.TicketId,
+                        principalTable: "Tickets",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Action_AuthorId",
+                table: "Action",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Action_TicketId",
+                table: "Action",
+                column: "TicketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Action_TicketId1",
+                table: "Action",
+                column: "TicketId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Action_TicketId2",
+                table: "Action",
+                column: "TicketId2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Action_TicketId3",
+                table: "Action",
+                column: "TicketId3");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachment_AuthorId",
+                table: "Attachment",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachment_TicketId",
+                table: "Attachment",
+                column: "TicketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_ClientId",
+                table: "Tickets",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_OpenId",
+                table: "Tickets",
+                column: "OpenId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Action_Tickets_TicketId",
+                table: "Action",
+                column: "TicketId",
+                principalTable: "Tickets",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Action_Tickets_TicketId1",
+                table: "Action",
+                column: "TicketId1",
+                principalTable: "Tickets",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Action_Tickets_TicketId2",
+                table: "Action",
+                column: "TicketId2",
+                principalTable: "Tickets",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Action_Tickets_TicketId3",
+                table: "Action",
+                column: "TicketId3",
+                principalTable: "Tickets",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Action_Techs_AuthorId",
+                table: "Action");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Action_Tickets_TicketId",
+                table: "Action");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Action_Tickets_TicketId1",
+                table: "Action");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Action_Tickets_TicketId2",
+                table: "Action");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Action_Tickets_TicketId3",
+                table: "Action");
+
+            migrationBuilder.DropTable(
+                name: "Attachment");
+
+            migrationBuilder.DropTable(
+                name: "Techs");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Action");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
