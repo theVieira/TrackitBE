eadff4a73b6ddc14b14b6c361b7bf2c9a965976f
theVieira <gabrielvieiraguimaraes4@gmail.com>
2025-04-01 22:26:25 -0400

    feat: add default user seed
    configure default user from appsettings. on initialize program.cs check if user exists, if so, creates it

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using DotNetEnv;
 using Trackit.Common.Extensions;
+using Trackit.Infra.Persistence;
+using Trackit.Seed;
 
 Env.Load();
 
@@ -8,5 +10,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddBuilderConfiguration();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    var config = services.GetRequiredService<IConfiguration>();
+    
+    var defaultUser = new CreateDefaultUser(context, config);
+    await defaultUser.Create();
+
+    Console.WriteLine("🚀");    
+}
 
 app.AddAppConfiguration();
---
+++ Seed/CreateDefaultUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trackit.Domain.Entities;
+using Trackit.Infra.Persistence;
+
+namespace Trackit.Seed;
+
+public class CreateDefaultUser(AppDbContext context, IConfiguration config)
+{
+    private readonly AppDbContext _context = context;
+    private readonly IConfiguration _config = config;
+    
+    public async Task Create()
+    {
+        var user = _config.GetSection("User").Get<UserConfig>();
+
+        if(user is null) throw new NullReferenceException("Default user configuration is null");
+        
+        var findUser = await _context.Techs.Where(x => x.Email == user.Email).FirstOrDefaultAsync();
+
+        if (findUser is not null) return;
+        
+        var tech = Tech.Factory.Create(user.Name, user.Password, user.Phone, user.Email, user.Role);
+        
+        await _context.Techs.AddAsync(tech);
+        await _context.SaveChangesAsync();
+    }
+}
+
+public class UserConfig
+{
+    public required string Name { get; init; }
+    public required string Password { get; init; }
+    public required string Email { get; init; }
+    public required string Phone { get; init; }
+    public Role Role { get; init; }
+}
