21c6e8f73bf911867af69a6ae5f1e3e0ca38b9d7
theVieira <gabrielvieiraguimaraes4@gmail.com>
2025-04-07 20:31:26 -0400

    fix: create dto to request, swagger fix IFormFile

--- Endpoints/Tech/EditTechAvatarEndpoint.cs
+++ Endpoints/Tech/EditTechAvatarEndpoint.cs
@@ -10,39 +10,48 @@ namespace Trackit.Endpoints.Tech;
 public class EditTechAvatarEndpoint : IEndpoint
 {
     public static void Map(IEndpointRouteBuilder app)
-        => app.MapPost("avatar", HandleAsync);
+        => app.MapPost("avatar", HandleAsync)
+            .Accepts<IFormFile>("multipart/form-data")
+            .Produces(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status401Unauthorized)
+            .WithName("EditTechAvatar")
+            .WithTags("Techs");
 
+    [Consumes("multipart/form-data")]
     private static async Task<IResult> HandleAsync(
-      
+        [FromForm]EditTechAvatarDto request,
+        [FromServices]ITokenManager tokenManager,
+        [FromServices]ITech techContext,
+        [FromServices]IAvatar avatarContext,
+        HttpContext httpContext
     )
     {
-        
+        var authorizationHeader = httpContext.Request.Headers["Authorization"].ToString();
+        var authorizationToken = authorizationHeader.Split("Bearer ");
+
+        var tokenClaim = tokenManager.GetClaimValue(JwtRegisteredClaimNames.Sub, authorizationToken[1]);
+ 
+        if(!Guid.TryParse(tokenClaim, out var id)) return Results.Unauthorized();
+
+        var tech = await techContext.FindByIdAsync(id);
+
+        if(tech is null) return Results.Unauthorized();
+
+        var avatar = Avatar.Factory.Create(Settings.UploadUrl, request.File.FileName, Settings.UploadPath);
+
+        await using (var fileStream = new FileStream(Settings.UploadPath, FileMode.Create))
+        {
+            await request.File.CopyToAsync(fileStream);
+        }
+
+        await avatarContext.AddAsync(avatar);
         
         return Results.Ok();
     }
 }
 
-//  [FromForm]IFormFile file,
-//       [FromServices]ITokenManager tokenManager,
-//       [FromServices]ITech techContext,
-//       [FromServices]IAvatar avatarContext
-
-//var authorizationHeader = HttpRequestHeader.Authorization.ToString();
-// var authorizationToken = authorizationHeader.Split("Bearer ");
-
-// var tokenClaim = tokenManager.GetClaimValue(JwtRegisteredClaimNames.Sub, authorizationToken[1]);
-// 
-// if(!Guid.TryParse(tokenClaim, out var id)) return Results.Unauthorized();
-
-// var tech = await techContext.FindByIdAsync(id);
-// 
-// if(tech is null) return Results.Unauthorized();
-// 
-// var avatar = Avatar.Factory.Create(Settings.UploadUrl, file.FileName, Settings.UploadPath);
-// 
-// await using (var fileStream = new FileStream(Settings.UploadPath, FileMode.Create))
-// {
-//     await file.CopyToAsync(fileStream);
-// }
-// 
-// await avatarContext.AddAsync(avatar);
+public class EditTechAvatarDto
+{
+    [FromForm]
+    public required IFormFile File { get; set; }
+}
