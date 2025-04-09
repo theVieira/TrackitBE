128b68e3fb60f2dc7c55c3b6f6f9d8f056045205
theVieira <gabrielvieiraguimaraes4@gmail.com>
2025-04-08 20:22:31 -0400

    fix: remove not necessary "new" for override method

--- Domain/Interfaces/ITicket.cs
+++ Domain/Interfaces/ITicket.cs
@@ -5,7 +5,7 @@ namespace Trackit.Domain.Interfaces;
 public interface ITicket : IBaseInterface<Ticket>
 {
     Task<BaseListResponse<Ticket>> ListByClientAsync(int skip, int take, TicketFilters filters, string clientName);
-    new Task<BaseListResponse<Ticket>> ListAsync(int skip, int take, TicketFilters filters);
+    Task<BaseListResponse<Ticket>> ListAsync(int skip, int take, TicketFilters filters);
 };
 
 public record TicketFilters(Status[] Status, Category[] Category, Priority[] Priority);
